47cef1cf02c21fc0f1fe38791b6a985241fef5dc
lokesh.patel <lokesh@apliteinfo.com>
2019-03-11 15:22:45 +0530

    Finished our loco Nav framework

---
+++ NavFramework/NavFramework/NavFramework.h
@@ -0,0 +1,19 @@
+//
+//  NavFramework.h
+//  NavFramework
+//
+//  Created by Apple on 11/03/19.
+//  Copyright © 2019 Lokesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NavFramework.
+FOUNDATION_EXPORT double NavFrameworkVersionNumber;
+
+//! Project version string for NavFramework.
+FOUNDATION_EXPORT const unsigned char NavFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavFramework/PublicHeader.h>
+
+
---
+++ NavFramework/NavFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  NavFramework
+//
+//  Created by Apple on 11/03/19.
+//  Copyright © 2019 Lokesh. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did some stuff, here you go homeboi!"
+    }
+}
